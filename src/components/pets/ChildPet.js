66dc4272dd96706a586b05a5b5bd8cbfc813c4fd
aborgra <audrey.borgra@gmail.com>
2020-02-05 11:14:15 -0600

    chore names show in relation to pet

---
+++ src/components/pets/ChildPet.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default ({pet, history}) => {
+
+return (
+    <section className="petCard">
+        <h3 className="pet__name">{pet.name}</h3>
+        {/* <img className="pet__pic">{pet.pic}</img> */}
+        <ul className="pet__chores">Chores:
+ 
+        {pet.foundChoresArray.map(fca => (
+          <li>{fca.chores.name}</li>))}
+        </ul>
+        
+
+    </section>
+)}
+
+
+
+
+
