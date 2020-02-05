16acdae78484785c41fa7b4d1e92cc41c50afcda
aborgra <audrey.borgra@gmail.com>
2020-02-05 09:40:21 -0600

    filters chores by user and pet

--- src/components/pets/ChildPetList.js
+++ src/components/pets/ChildPetList.js
@@ -1,52 +1,39 @@
 import React, { useContext } from "react";
 import "./ChildPet.css";
 import { PetContext } from "./PetProvider";
-import { ChildPet } from "./ChildPet"
-import { UserContext } from "../users/UserProvider"
+import { ChildPet } from "./ChildPet";
+import { UserContext } from "../users/UserProvider";
 import { KidPetChoreContext } from "../chores/KidPetChoreProvider";
-
 
 export default props => {
   const { users } = useContext(UserContext);
   const { pets } = useContext(PetContext);
-  const { kidPetChores } = useContext(KidPetChoreContext);
-  console.log(pets)
+  const { kidPetChores } = useContext(KidPetChoreContext)
 
-  const activeUserId = parseInt(localStorage.getItem("fido_user"))
+console.log("pets", pets)
+
+  const activeUserId = parseInt(localStorage.getItem("fido_user"));
   const activeUser = users.find(user => user.id === activeUserId) || {}
-  let allChildPetChores = []
-  const petChoresArray = pets.filter(
-    pet => {
-      let singleChoreObj = pet.kidPetChores.map(kpc => {
-        console.log(kpc)
-        if (kpc.userId === activeUserId){
-          return kpc}
-      })
-      if(singleChoreObj !== undefined && singleChoreObj !== []){return singleChoreObj} 
+  const petChoresArray =
+    pets.map(pet => {
+      let allChildPetChores = []
+      let foundPetChores = pet.kidPetChores.map(kpc => {
+        if (kpc.userId === activeUserId) {
+          console.log("kpc", kpc)
+          allChildPetChores.push(kpc)}}) || []
+      pet.foundChoresArray = allChildPetChores
+      return pet}) || [];
 
-    }
-    ) || []
+ 
 
-    let filteredPetChoresArray = []
-    petChoresArray.map(petChore => {
-      if(petChore.kidPetChores.length > 0){
-        filteredPetChoresArray.push(petChore)
-      }
-    })
+  console.log("allChildPetChores", petChoresArray);
 
-    console.log("petChoresArray", petChoresArray)
-    console.log("filtered", filteredPetChoresArray)
-    // returning all chores of pets with at least one chore with userId of activeUser
 
-  
   return (
     <div className="childPets">
-      
-        {allChildPetChores.map(kpc => {
-       
-
+      {petChoresArray.map(kpc => {
         return (
-        <div>{kpc.kidPetChores.dueDate}</div>
+          <div>{kpc.kidPetChores.dueDate}</div>
           // <ChildPet
           //   key={kpc.id}
           //   kpc={kpc}
