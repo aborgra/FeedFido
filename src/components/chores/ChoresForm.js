e982ecbaf29f0bc22cdf0c8a05897673f7a68b7b
aborgra <audrey.borgra@gmail.com>
2020-02-07 22:44:50 -0600

    able to add new chore with dialog, need to fix alert statement

--- src/components/chores/ChoresForm.js
+++ src/components/chores/ChoresForm.js
@@ -1,8 +1,13 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { UserContext } from "../users/UserProvider";
 import { ChoreContext } from "./ChoreProvider";
 import { KidPetChoreContext } from "./KidPetChoreProvider";
 import { Container } from "@material-ui/core";
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 export default props => {
   const { addChore, chores, editChore } = useContext(ChoreContext);
@@ -14,8 +19,8 @@ export default props => {
   } = useContext(KidPetChoreContext);
 
   const { users } = useContext(UserContext);
-
-  const [buttonClicked, setButtonClicked] = useState(false);
+  const newChore = useRef("")
+  // const [ButtonClicked, setButtonClicked] = useState(false);
   const [singleChore, setSingleChore] = useState({});
   const activeUserId = parseInt(localStorage.getItem("fido_user"));
 
@@ -47,6 +52,20 @@ export default props => {
     setDefaults();
   }, [kidPetChores]);
 
+  const constructNewChore = () => {
+    let choreName = newChore.current.value
+    console.log("choreName", choreName)
+    // const foundChore = chores.find(chore => chore.name === choreName) || {}
+    // console.log("foundChore", foundChore)
+    // if(foundChore === {}){
+    addChore({
+      name: choreName
+    }).then(handleClose)
+  // }else(
+  //   window.alert("Chore already exists")
+  // )
+}
+
   const constructNewKidPetChore = () => {
     if (editMode) {
       editKidPetChore({
@@ -75,6 +94,52 @@ export default props => {
     }
   };
 
+  function SimpleDialog(props) {
+    // const classes = useStyles();
+    const { onClose, open } = props;
+    const handleClose = () => {
+      onClose(singleChore);
+    };
+  
+    // const handleListItemClick = value => {
+    //   onClose(value);
+    // };
+    
+    return (
+      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+        <DialogTitle id="simple-dialog-title">Add Chore Option</DialogTitle>
+        <form>
+          <input type="text" id="standard" required autoFocus label="Add a Chore Option" variant="outlined" name="name" defaultValue={""} ref={newChore}  
+          />
+        </form>
+        <Button className="saveChoreButton" onClick={(evt) => {
+          evt.preventDefault();
+          constructNewChore()
+        }}>
+          Save Chore
+        </Button>
+      </Dialog>
+    );
+  }
+
+  const [open, setOpen] = React.useState(false);
+  // const [selectedValue, setSelectedValue] = React.useState({});
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = value => {
+    setOpen(false);
+    // setSelectedValue(value);
+  };
+
+  // const handleControlledChoreInputChange = event => {
+  //   const addedChore = Object.assign({}, selectedValue);
+  //   addedChore[event.target.name] = event.target.value;
+  //   setSelectedValue(addedChore);
+  // };
+
   return (
     <>
       <Container>
@@ -98,6 +163,8 @@ export default props => {
                 </option>
               ))}
             </select>
+            <Button className="addChoreOption" onClick={handleClickOpen}>Add Chore</Button>
+            <SimpleDialog  open={open} onClose={handleClose} />
           </fieldset>
           <fieldset>
             <select
@@ -140,7 +207,7 @@ export default props => {
           </fieldset>
 
           <div className="saveButtonContainer">
-            <button
+            <Button
               type="submit"
               onClick={evt => {
                 evt.preventDefault();
@@ -148,14 +215,14 @@ export default props => {
               }}
               className="btn btn-primary"
             >
-              {editMode ? "Save changes" : "Add Chore"}
-            </button>
-            <button
+              {editMode ? "Save changes" : "Save Chore"}
+            </Button>
+            <Button
               className=" closeBtn btn btn-light"
               onClick={() => props.history.push("/")}
             >
               Close
-            </button>
+            </Button>
           </div>
         </form>
       </Container>
