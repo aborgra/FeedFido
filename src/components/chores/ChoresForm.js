da77debaad0b70ba9555af8077123377f04712f4
aborgra <audrey.borgra@gmail.com>
2020-02-07 19:45:52 -0600

    added images to pet types

--- src/components/chores/ChoresForm.js
+++ src/components/chores/ChoresForm.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../users/UserProvider";
 import { ChoreContext } from "./ChoreProvider";
 import { KidPetChoreContext } from "./KidPetChoreProvider";
+import { Container } from "@material-ui/core";
 
 export default props => {
   const { addChore, chores, editChore } = useContext(ChoreContext);
@@ -76,86 +77,88 @@ export default props => {
 
   return (
     <>
-      <form className="choreForm">
-        <h2 className="choreForm__title">
-          {editMode ? "Edit Chore" : "Add Chore"}
-        </h2>
+      <Container>
+        <form className="choreForm">
+          <h2 className="choreForm__title">
+            {editMode ? "Edit Chore" : "Add Chore"}
+          </h2>
 
-        <fieldset>
-          <select
-            value={singleChore.choreId}
-            name="choreId"
-            id="choreId"
-            className="form-control"
-            onChange={handleControlledInputChange}
-          >
-            <option value="0">Select a Chore</option>
-            {chores.map(chore => (
-              <option key={chore.id} value={chore.id}>
-                {chore.name}
-              </option>
-            ))}
-          </select>
-        </fieldset>
-        <fieldset>
-          <select
-            value={singleChore.userId}
-            name="userId"
-            id="userId"
-            className="form-control"
-            onChange={handleControlledInputChange}
-          >
-            <option value="0">Select a Child</option>
-            {userChildren.map(child => (
-              <option key={child.id} value={child.id}>
-                {child.userName}
-              </option>
-            ))}
-          </select>
-        </fieldset>
+          <fieldset>
+            <select
+              value={singleChore.choreId}
+              name="choreId"
+              id="choreId"
+              className="form-control"
+              onChange={handleControlledInputChange}
+            >
+              <option value="0">Select a Chore</option>
+              {chores.map(chore => (
+                <option key={chore.id} value={chore.id}>
+                  {chore.name}
+                </option>
+              ))}
+            </select>
+          </fieldset>
+          <fieldset>
+            <select
+              value={singleChore.userId}
+              name="userId"
+              id="userId"
+              className="form-control"
+              onChange={handleControlledInputChange}
+            >
+              <option value="0">Select a Child</option>
+              {userChildren.map(child => (
+                <option key={child.id} value={child.id}>
+                  {child.userName}
+                </option>
+              ))}
+            </select>
+          </fieldset>
 
-        <fieldset>
-          <select
-            value={singleChore.day}
-            name="day"
-            id="day"
-            className="form-control"
-            onChange={handleControlledInputChange}
-          >
-            <option value="0">Sunday</option>
-            <option value="1">Monday</option>
-            <option value="2">Tuesday</option>
-            <option value="3">Wednesday</option>
-            <option value="4">Thursday</option>
-            <option value="5">Friday</option>
-            <option value="6">Saturday</option>
-          </select>
-        </fieldset>
+          <fieldset>
+            <select
+              value={singleChore.day}
+              name="day"
+              id="day"
+              className="form-control"
+              onChange={handleControlledInputChange}
+            >
+              <option value="0">Sunday</option>
+              <option value="1">Monday</option>
+              <option value="2">Tuesday</option>
+              <option value="3">Wednesday</option>
+              <option value="4">Thursday</option>
+              <option value="5">Friday</option>
+              <option value="6">Saturday</option>
+            </select>
+          </fieldset>
 
-        <fieldset>
-          <label>Recurring Event</label>
-          <input type="checkbox"/>
-        </fieldset>
+          <fieldset>
+            <label>Recurring Event</label>
+            <input type="checkbox" />
+          </fieldset>
 
-        <div className="saveButtonContainer">
-          <button
-            type="submit"
-            onClick={evt => {
-              evt.preventDefault();
-              constructNewKidPetChore();
-            }}
-            className="btn btn-primary"
-          >
-            {editMode ? "Save changes" : "Add Chore"}
-          </button>
-          <button
-            className=" closeBtn btn btn-light"
-            onClick={() => props.history.push("/")}
-          >
-            Close
-          </button>
-        </div>
-      </form>
+          <div className="saveButtonContainer">
+            <button
+              type="submit"
+              onClick={evt => {
+                evt.preventDefault();
+                constructNewKidPetChore();
+              }}
+              className="btn btn-primary"
+            >
+              {editMode ? "Save changes" : "Add Chore"}
+            </button>
+            <button
+              className=" closeBtn btn btn-light"
+              onClick={() => props.history.push("/")}
+            >
+              Close
+            </button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 };
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 import { UserContext } from "../users/UserProvider";
+import Button from '@material-ui/core/Button';
+
 
 export default props => {
 
@@ -12,17 +14,21 @@ const { users } = useContext(UserContext)
 
   return (
     <div className="navbar">
-     <h3 className="navbar__message">{activeUser.userName} - Welcome to Feed Fido!</h3>
+      <img className="backIcon"src={require("../pics/undo.svg")}
+      // onClick={props.history.push("/")}
+      />
+      <h2 className="siteName">Feed Fido</h2>
+     {/* <h3 className="navbar__message">{activeUser.userName} - Welcome to Feed Fido!</h3> */}
 {
     localStorage.getItem("fido_user")
         ?
-          <button className= "logoutButton btn btn-light " onClick={e => {
+          <Button variant="contained" className= "logoutButton" onClick={e => {
                     e.preventDefault()
                     localStorage.removeItem("fido_user")
                     props.history.push("/")
                 }}>
             Log Out
-          </button>
+          </Button>
   
        
         : ""
