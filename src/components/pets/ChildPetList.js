e142ac42428bde36dc5b83cd9245a893af5c9945
aborgra <audrey.borgra@gmail.com>
2020-02-14 11:03:03 -0600

    fixed message display on login

--- src/components/pets/ChildPetList.js
+++ src/components/pets/ChildPetList.js
@@ -9,50 +9,59 @@ import { ChoreContext } from "../chores/ChoreProvider";
 export default props => {
   const { users } = useContext(UserContext);
   const { pets } = useContext(PetContext);
-  const { kidPetChores } = useContext(KidPetChoreContext)
-  const { chores } = useContext(ChoreContext)
-  const today = new Date()
-  const dayOfWeek = today.getDay()
+  const { kidPetChores } = useContext(KidPetChoreContext);
+  const { chores } = useContext(ChoreContext);
+  const today = new Date();
+  const dayOfWeek = today.getDay();
   const activeUserId = parseInt(localStorage.getItem("fido_user"));
   var options = { year: "numeric", month: "numeric", day: "numeric" };
 
-  const [scheduledDate, x] = today.toLocaleString("en-US", options)
-  .split(",")
-  
-  
-  
+  const [scheduledDate, x] = today.toLocaleString("en-US", options).split(",");
+
   const allPetChoresArray =
     pets.map(pet => {
-      let allChildPetChores = []
-      let foundPetChores = pet.kidPetChores.filter(kpc => (kpc.day === dayOfWeek && !(kpc.hasOwnProperty("schedDate"))) || kpc.schedDate === scheduledDate)
-        let foundkpc = foundPetChores.map(fkpc => {
+      let allChildPetChores = [];
+      let foundPetChores = pet.kidPetChores.filter(
+        kpc =>
+          (kpc.day === dayOfWeek && !kpc.hasOwnProperty("schedDate")) ||
+          kpc.schedDate === scheduledDate
+      );
+      let foundkpc =
+        foundPetChores.map(fkpc => {
           // console.log(fkpc, "fkpc")
-         if (fkpc.userId === activeUserId && fkpc.isCompleted === false) {
-         fkpc.chores = chores.find(chore => fkpc.choreId === chore.id)
-          allChildPetChores.push(fkpc)}}) || []
-      pet.foundChoresArray = allChildPetChores
-      return pet}) || [];
+          if (fkpc.userId === activeUserId && fkpc.isCompleted === false) {
+            fkpc.chores = chores.find(chore => fkpc.choreId === chore.id);
+            allChildPetChores.push(fkpc);
+          }
+        }) || [];
+      pet.foundChoresArray = allChildPetChores;
+      return pet;
+    }) || [];
 
-      const filteredPetChores = allPetChoresArray.filter (pc => pc.foundChoresArray.length !== 0) || []
+  const filteredPetChores =
+    allPetChoresArray.filter(pc => pc.foundChoresArray.length !== 0) || [];
 
+  const childListRender = () => {
+    if (filteredPetChores.length === 0) {
+      return <div className="childMessage">"You Have No Chores For Today"</div>;
+    } else if (filteredPetChores === []){}
+    else {
+      return (
+        <>
+          {filteredPetChores.map(fpc => {
+            return(
+            <ChildPet {...props} key={fpc.id} pet={fpc} />)
+          })}
+        </>
+      );
+    }
+  };
 
-const childList = filteredPetChores.map(fpc => {
-  
   return (
-    <ChildPet {...props}
-      key={fpc.id}
-      pet={fpc}
-    />
-  );
-})
-
-const message = "You Have No Chores For Today"
- 
-
-  return (
-    <div className="childPets">
-      {filteredPetChores.length === 0 ? (<div className="childMessage">{message}</div>):(<div className="childListContainer">{childList}</div>)}
-      
-    </div>
+    <>
+      <div className="childPets">
+        <div className="childListContainer">{childListRender()}</div>
+      </div>
+    </>
   );
 };
